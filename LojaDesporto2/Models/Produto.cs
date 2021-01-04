436da73c8a025493844e97c1a53a6f2f9665b272
João Vieira <feijoca96@hotmail.com>
2021-01-04 16:08:59 +0000

    Adicionado modelo para representar um produto

---
+++ LojaDesporto2/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaDesporto2.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
