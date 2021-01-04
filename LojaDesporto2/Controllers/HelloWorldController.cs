9d1791536fae8677e463c2b8e3f586615c0599d8
João Vieira <feijoca96@hotmail.com>
2021-01-04 15:19:25 +0000

    Add project files.

---
+++ LojaDesporto2/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaDesporto2.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string BemVindo()
+        {
+            return "Bem Vindo!!";
+        }
+    }
+}
---
+++ LojaDesporto2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LojaDesporto2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
